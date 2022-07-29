4bb9d048905e1b66076157719c6d84359ada69f3
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-07-29 23:39:00 +0200

    Make it work on Windows by not using a bash script file

--- importtime.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-code="$1"
-threshold="$2"
-
-importtime="$(python -X importtime -c "${code}" 2>&1 | tail -1 | sed 's/^.*| *\([0-9][0-9]*\) *|.*$/\1/g')"
-echo "Import time for '${code}' is ${importtime} and threshold is ${threshold}"
-test "${importtime}" -le "${threshold}"
